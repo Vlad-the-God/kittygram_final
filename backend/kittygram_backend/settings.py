81660e441135bde1fc851ec61485459f17754229
Vlad_the_God <koordinator.01@mail.ru>
2024-09-28 13:02:31 +0300

    Fix secret key check

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,7 +11,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY') if not None else get_random_secret_key()
+SECRET_KEY = os.getenv('SECRET_KEY') if os.getenv('SECRET_KEY') else get_random_secret_key()
 
 DEBUG = check_debug(os.getenv('DEBUG'))
 
